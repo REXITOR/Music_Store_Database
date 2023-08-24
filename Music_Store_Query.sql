b63471c16046fc6cd46c1c7950ca957c9cf0f373
Mohammadreza <m2136893@gmail.com>
2023-08-24 15:01:22 +0330

    Change Queries

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -104,7 +104,8 @@ ORDER BY miliseconds DESC;
 
 /* Question Set 3 */
 
-/* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent. */
+/* Q1: Which artist has each customer paid the most for? (based on invoice_line table) 
+Write a query to return customer name, artist name and total spent. */
 
 WITH best_selling_artist AS (
 	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
@@ -127,8 +128,8 @@ GROUP BY 1,2,3,4
 ORDER BY 5 DESC;
 
 
-/* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre with the highest amount of purchases, 
-Write a query that returns each country along with the top Genre. For countries where the maximum number of purchases is shared return all Genres. */
+/* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre with the highest amount of purchases(quantity),
+Write a query that returns each country along with the top Genre. */
 
 WITH popular_genre AS 
 (
@@ -145,9 +146,8 @@ WITH popular_genre AS
 SELECT * FROM popular_genre WHERE RowNo <= 1
 
 
-/* Q3: Write a query that determines the customer that has spent the most on music for each country, 
-Write a query that returns the country along with the top customer and how much they spent, 
-For countries where the top amount spent is shared, provide all customers who spent this amount. */
+/* Q3: Write a query that determines the customer that has spent(total) the most on music for each country, 
+Write a query that returns the country along with the top customer and how much they spent. */
 
 WITH Customter_with_country AS (
 		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending,
